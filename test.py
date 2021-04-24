215d69caa8dd1e688f3f05f3631c1b597439687b
SevenNanami <79927309+SevenNanami@users.noreply.github.com>
2021-04-24 11:12:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,92 +1,93 @@
-# 域名检测
-# train.txt, test.txt, result.txt
-# 每条域名选取三种特征：域名长度，域名中的数字数量，域名字符熵
-# 机器学习采用随机森林算法
-
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import math
-from pandas.core.frame import DataFrame
 
+domainlist = []
+class Domain:
+    def __init__(self,_name,_label,_length,_numbers,_entropy):
+        self.name = _name
+        self.label = _label
+        self.length=_length
+        self.numbers=_numbers
+        self.entropy=_entropy
 
-# 计算字符熵
-def LettersEntropy(str):
-    # 统计每个字符出现的次数
-    letters, counts = np.unique(list(str), return_counts=True)
-    total = sum(counts)
-    entropy = 0.0
-    for i in range(len(counts)):
-        prob = counts[i]/total
-        entropy += - prob * math.log(2, prob)
-    return entropy
+    def returnData(self):
+        return [self.length, self.numbers, self.entropy]
 
-def NumCollect(str):
-    # 统计每条域名中的数字个数
-    total = 0
-    for i in str:
-        if i.isdigit():
-            total +=1
-    return total
+    def returnLabel(self):
+        if self.label == "dga":
+            return 1
+        else:
+            return 0
+
+def initData(filename):
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("#") or line =="":
+                continue
+            tokens = line.split(",")
+            name = tokens[0]
+            label = tokens[1]
+            length= len(name)
+            num=0
+            entro=0 
+            result={}
+            frequency={}
+            for i in name:
+                result[i]=name.count(i)
+                frequency[i]=float(result[i])/length
+                entro-=1*(frequency[i])*math.log(frequency[i],2)
+                if i.isdigit():
+                        num +=1
+            numbers= num  
+            entropy = entro
+            domainlist.append(Domain(name,label,length,numbers,entropy))
+
+def main():
+    initData("train.txt")
+    featureMatrix = []
+    labelList = []
+    for item in domainlist:
+        featureMatrix.append(item.returnData())
+        labelList.append(item.returnLabel())
+
+    clf = RandomForestClassifier(random_state=0)
+    clf.fit(featureMatrix,labelList)
+
+    file1=open("test.txt")
+    file2=open("result.txt","w")
+    for line in file1:
+        line = line.strip()
+        if line.startswith("#") or line =="":
+            continue
+        tokens_test = line.split(",")
+        name_test = tokens_test[0]
+        length_test = len(name_test)
+        num_test=0
+        entro_test=0     
+        result_test={} 
+        frequency_test={}
+        for i in name_test:
+            result_test[i]=name_test.count(i)
+            frequency_test[i]=float(result_test[i])/length_test
+            entro_test-=1*(frequency_test[i])*math.log(frequency_test[i],2)
+            if i.isdigit():
+                num_test +=1
+        numbers_test= num_test  
+        entropy_test = entro_test
+        if clf.predict([[length_test,numbers_test,entropy_test]])==0:
+            label_test1 = "notdga"
+            content=line+","+label_test1
+            file2.write(content)
+            file2.write("\n")
+        if clf.predict([[length_test,numbers_test,entropy_test]])==1:
+            label_test2 = "dga"
+            content=line+","+label_test2
+            file2.write(content)
+            file2.write("\n")
+    file1.close
+    file2.close
 
 if __name__ == '__main__':
-    # 创建3个特征列表，1个种类列表
-    DomainLen = []
-    Numbers = []
-    Entropy = []
-    Type = []
-    # 读取train.txt文件
-    f = open(r'train.txt')
-    for line in f:
-        line = line.strip()
-        if line == "":
-            continue
-        tokens = line.split(",")
-        domain = tokens[0]
-        label = tokens[1]
-        DomainLen.append(len(domain))
-        Numbers.append(NumCollect(domain))
-        Entropy.append(LettersEntropy(domain))
-        if label == 'notdga':
-            Type.append(0)
-        else:
-            Type.append(1)
-    traindata = {'Length': DomainLen, 'NumInDomain': Numbers, 'Entropy': Entropy, 'Type': Type}
-    traindata = DataFrame(traindata)
-    # 设置训练数据集
-    y = traindata.Type
-    x = traindata.drop('Type', axis=1)
-    xtrain = x
-    ytrain = y
-    # 读取test.txt文件
-    testDomainLen = []
-    testNumbers = []
-    testEntropy = []
-    testType = []
-    testDomainName = []
-    TestFile = open(r'test.txt')
-    for line in TestFile:
-        line = line.strip()
-        if line == "":
-            continue
-        testDomainName.append(line)
-        testDomainLen.append(len(line))
-        testNumbers.append(NumCollect(line))
-        testEntropy.append(LettersEntropy(line))
-    testdata = {'Length': testDomainLen, 'NumInDomain': testNumbers, 'Entropy': testEntropy}
-    testdata = DataFrame(testdata)
-    # 设置测试数据集
-    xtest = testdata
-    # 实例化
-    rfc = RandomForestClassifier()
-    # 用训练集数据训练模型
-    rfc = rfc.fit(xtrain, ytrain)
-    ytest = rfc.predict(xtest)
-    for i in range(len(ytest)):
-        if ytest[i] == 0:
-            testType.append('notdga')
-        else:
-            testType.append('dga')
-    with open('result.txt', 'w+') as ResultFile:
-        for i in range(len(testDomainName)):
-            line = testDomainName[i] + ',' + testType[i] + '\n'
-            ResultFile.write(line)
+    main()
